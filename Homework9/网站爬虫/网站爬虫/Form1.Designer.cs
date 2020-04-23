103503346d83f8817decfa9ed45ee2f60cba9875
w824449964 <50574059+w824449964@users.noreply.github.com>
2020-04-23 21:25:55 +0800

    爬虫程序

---
+++ Homework9/网站爬虫/网站爬虫/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace 网站爬虫
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbStartUrl = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.dgvResult = new System.Windows.Forms.DataGridView();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.numIndex = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.URL = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbStartUrl
+            // 
+            this.tbStartUrl.Location = new System.Drawing.Point(34, 24);
+            this.tbStartUrl.Name = "tbStartUrl";
+            this.tbStartUrl.Size = new System.Drawing.Size(510, 25);
+            this.tbStartUrl.TabIndex = 0;
+            this.tbStartUrl.Text = "http://www.cnblogs.com/dstang2000/";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(565, 25);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "启动爬虫";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // dgvResult
+            // 
+            this.dgvResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResult.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.numIndex,
+            this.URL,
+            this.Status});
+            this.dgvResult.Location = new System.Drawing.Point(12, 107);
+            this.dgvResult.Name = "dgvResult";
+            this.dgvResult.RowHeadersWidth = 51;
+            this.dgvResult.RowTemplate.Height = 27;
+            this.dgvResult.Size = new System.Drawing.Size(776, 338);
+            this.dgvResult.TabIndex = 2;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(45, 71);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(67, 15);
+            this.lblStatus.TabIndex = 3;
+            this.lblStatus.Text = "爬虫状态";
+            // 
+            // numIndex
+            // 
+            this.numIndex.DataPropertyName = "numIndex";
+            this.numIndex.HeaderText = "编号";
+            this.numIndex.MinimumWidth = 6;
+            this.numIndex.Name = "numIndex";
+            this.numIndex.Width = 125;
+            // 
+            // URL
+            // 
+            this.URL.DataPropertyName = "URL";
+            this.URL.HeaderText = "URL网址";
+            this.URL.MinimumWidth = 6;
+            this.URL.Name = "URL";
+            this.URL.Width = 300;
+            // 
+            // Status
+            // 
+            this.Status.DataPropertyName = "Status";
+            this.Status.HeaderText = "状态";
+            this.Status.MinimumWidth = 6;
+            this.Status.Name = "Status";
+            this.Status.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 468);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.dgvResult);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.tbStartUrl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResult)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbStartUrl;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.DataGridView dgvResult;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.DataGridViewTextBoxColumn numIndex;
+        private System.Windows.Forms.DataGridViewTextBoxColumn URL;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Status;
+    }
+}
+
---
+++ Homework9/网站爬虫/网站爬虫/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 网站爬虫
+{
+    public partial class Form1 : Form
+    {
+        BindingSource resultBindingSource = new BindingSource();
+        //使用BindingSource绑定数据，以便爬虫状态一更新就可以显示在dataGridView上
+        Crawler mycrawler = new Crawler();
+        Thread thread = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            dgvResult.DataSource = resultBindingSource;
+            //绑定相关的事件响应
+            mycrawler.PageDownloaded += Download;
+            mycrawler.CrawlerStopped += StopCrawing;
+        }
+
+        //相应Action
+        private void StopCrawing(Crawler obj)
+        {
+            Action action = () => lblStatus.Text = "爬虫已停止";
+            if (this.InvokeRequired)
+            {
+                this.Invoke(action);
+            }
+            else
+            {
+                action();
+            }
+        }
+
+        private void Download(Crawler crawler, string url, string info)
+        {
+            var pageInfo = new { numIndex = resultBindingSource.Count + 1, URL = url, Status = info };
+            Action action = () => { resultBindingSource.Add(pageInfo); };
+            if (this.InvokeRequired)
+            {
+                this.Invoke(action);
+            }
+            else
+            {
+                action();
+            }
+        }
+
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            resultBindingSource.Clear();
+            mycrawler.StartURL = tbStartUrl.Text;
+
+            Match match = Regex.Match(mycrawler.StartURL, Crawler.urlParseRegex);
+            if (match.Length == 0) return;
+            string host = match.Groups["host"].Value;
+            mycrawler.HostFilter = "^" + host + "$";
+            mycrawler.FileFilter = ".html?$";
+
+            if (thread != null)
+            {
+                thread.Abort();
+            }
+            thread = new Thread(mycrawler.Start);
+            thread.Start();
+            lblStatus.Text = "爬虫已启动....";
+
+        }
+
+    }
+}
